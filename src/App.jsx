d9c8d401cd19a1643a46e765a5b150aca2539475
anjudixit <163434535+anjudixit@users.noreply.github.com>
2025-04-27 19:54:26 +0530

    updated again

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import Navbar from './components/Navbar'
 import { v4 as uuidv4 } from 'uuid';
-import { AnimatePresence, Motion } from 'framer-motion';
+import { AnimatePresence} from 'framer-motion';
 
 function App() {
   const [todo, setTodo] = useState('');
